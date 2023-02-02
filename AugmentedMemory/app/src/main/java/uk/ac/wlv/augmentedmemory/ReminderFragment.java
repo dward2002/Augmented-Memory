e13940e600381abfde4e7a8e4b99ac4f5d0b5fb8
dward2002 <d.ward4@wlv.ac.uk>
2023-02-02 14:43:18 +0000

    Created ReminderFragment.java which is currently an empty fragment

---
+++ AugmentedMemory/app/src/main/java/uk/ac/wlv/augmentedmemory/ReminderFragment.java
@@ -0,0 +1,12 @@
+package uk.ac.wlv.augmentedmemory;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+public class ReminderFragment extends Fragment {
+    @Override
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+    }
+}
