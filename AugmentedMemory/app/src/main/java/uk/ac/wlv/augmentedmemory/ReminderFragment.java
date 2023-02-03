cbd948c80c042c820668d96f1a537ed6ff984b18
dward2002 <d.ward4@wlv.ac.uk>
2023-02-03 11:21:02 +0000

    altered to get the data from database for that specific reminder

--- AugmentedMemory/app/src/main/java/uk/ac/wlv/augmentedmemory/ReminderFragment.java
+++ AugmentedMemory/app/src/main/java/uk/ac/wlv/augmentedmemory/ReminderFragment.java
@@ -1,16 +1,43 @@
 package uk.ac.wlv.augmentedmemory;
 
+import static uk.ac.wlv.augmentedmemory.MainActivity.MESSAGES_CHILD;
+
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class ReminderFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
+        String reminderId = (String) getActivity().getIntent().getSerializableExtra(ReminderActivity.EXTRA_REMINDER_ID);
+        DatabaseReference mFirebaseDeleteReference = FirebaseDatabase.getInstance().getReference()
+                .child("messages").child(reminderId);
+        mFirebaseDeleteReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                for(DataSnapshot data : snapshot.getChildren()) {
+                    Reminder post = data.getValue(Reminder.class);
+                    Log.d("www",post.getmTitle());
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
     }
 
     @Override
