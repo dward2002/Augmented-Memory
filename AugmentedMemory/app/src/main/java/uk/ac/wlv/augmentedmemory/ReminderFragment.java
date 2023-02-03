48cf3b3a6d9286dc05bd426aa37f4fd000d4f11b
dward2002 <d.ward4@wlv.ac.uk>
2023-02-03 11:02:39 +0000

    added onCreateView method

--- AugmentedMemory/app/src/main/java/uk/ac/wlv/augmentedmemory/ReminderFragment.java
+++ AugmentedMemory/app/src/main/java/uk/ac/wlv/augmentedmemory/ReminderFragment.java
@@ -1,6 +1,9 @@
 package uk.ac.wlv.augmentedmemory;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 import androidx.fragment.app.Fragment;
 
@@ -8,5 +11,11 @@ public class ReminderFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_reminder, container, false);
+        return v;
     }
 }
